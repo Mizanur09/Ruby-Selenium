42e333872805ca972c0f2d9adb04b3890db08224
Mizanur Rahman <mizanur.rahman0918@gmail.com>
2018-03-25 16:10:18 -0400

    Update initializeConfigurations, added different environment veriables

--- acceptance_test/module/DriverUtility.rb
+++ acceptance_test/module/DriverUtility.rb
@@ -6,20 +6,32 @@ require 'selenium-webdriver'
 module DriverUtility
   FIREFOX_PROFILE_PATH = File.expand_path('.') + "/acceptance_test/selenium-ruby/firefoxProfile"
 
-  def initializeConfigurations(base_url)
+  $DEMO = "https://provide the URL of Demo environment"
+  $LOCAL ="https://provide the URL of Local environment"
+  $REMOTE = "https://provide the URL of Remote environment"
 
-    # Selenium::WebDriver::Chrome.driver_path='path of the chrome.exe'
-    # @driver = Selenium::WebDriver.for :chrome
-    # @driver = Selenium::WebDriver.for :safari
 
-    @driver = Selenium::WebDriver.for :firefox
-    @driver.manage.window.maximize
-    @driver.manage.delete_all_cookies
-    @driver.get(base_url + "/")
-    @accept_next_alert = true
-    @verification_errors = []
+
+  def initializeConfigurations(base_url = $LOCAL) # change the environment according to where the test need to be run. Example if you like to run into remote environment then base_url = $Remote
+    if base_url == $REMOTE
+      browserCapabilities = Selenium::WebDriver::Remote::Capabilities.firefox(accept_insecure_certs: true)
+      @driver = Selenium::WebDriver.for(:remote,
+                                        url: "http://localhost:4444/wd/hub",
+                                        desired_capabilities: browserCapabilities)
+    elsif base_url == $LOCAL
+
+      # Selenium::WebDriver::Chrome.driver_path='path of the chrome.exe'
+      # @driver = Selenium::WebDriver.for :chrome
+      # @driver = Selenium::WebDriver.for :safari
+
+      @driver = Selenium::WebDriver.for :firefox
     end
-  
+    @driver.manage.timeouts.implicit_wait = 10
+    @driver.manage.window.maximize
+    @verification_errors = []
+    @driver.get(base_url)
+  end
+
   def quitDriver()
     @driver.quit
     expect(@verification_errors).to eq([])
