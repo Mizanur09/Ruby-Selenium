d18772388ebb636a30a63db230f6bcaa55bb4e59
Mizanur Rahman <mizanur.rahman0918@gmail.com>
2018-03-25 16:34:17 -0400

    added the allure test result path

--- acceptance_test/Helpers_pages/rspec_helper.rb
+++ acceptance_test/Helpers_pages/rspec_helper.rb
@@ -10,3 +10,12 @@ RSpec.configure do |c|
   c.filter_run_excluding :acceptance => true
   c.filter_run_excluding :smoketest => true
 end
+
+RSpec.configure do |config|
+  config.include AllureRSpec::Adaptor
+end
+
+AllureRSpec.configure do |config|
+  config.output_dir = File.join(Dir.home(), "~/Ruby-Selenium/allure-report/allure_results") # add the path where you want to export the data.
+  config.clean_dir = true
+end
