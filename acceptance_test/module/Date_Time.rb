2eabc17de2bffefbc8d95b1784f5ee819efdbe0e
mizanur09 <mizanur.rahman0918@gmail.com>
2018-10-03 23:29:03 -0400

    Added date and time format methods

---
+++ acceptance_test/module/Date_Time.rb
@@ -0,0 +1,113 @@
+require_relative '../Helpers_pages/globalized'
+include Globalized
+
+class DateSetup
+
+##############################################   DATE  #############################################
+  # Date set up
+  @oneDayAgo = getDateNthDaysAgo(-1)
+  @today = getDateNthDaysAgo(0)
+  @tomorrow = getDateNthDaysFromNow(1)
+
+  # Month set up
+  @LastMonth = getDateNthMonthsAgo(-1)
+  @CurrentMonth = getDateNthMonthsAgo(0)
+  @NextMonth = getDateNthMonthsFromNow(1)
+
+  # Year set up
+  @LastYear = getDateNthYearsAgo(-1)
+  CurrentYear = getDateNthYearsAgo(0)
+  @NextYear = getDateNthYeasFromNow(1)
+
+  def setDate(dateStr, cssPath)
+    findElement(:css, cssPath).clear
+    findElement(:css, cssPath).send_keys(dateStr)
+  end
+
+  # Verify the date format in from the field
+  def isDateFormatValid?(dateStr)
+    begin
+      dateObj = DateTime.strptime(dateStr, format= "%m/%d/%Y")
+      puts "[isDateFormatValid?] Date is " + dateObj.to_s
+      return true
+    rescue Exception=>e
+      return false
+    end
+  end
+
+  # DATE [days]
+  def getDateNthDaysAgo(numberOfDaysAgo)
+    # def getDateNthDaysAgo(numberOfDaysAgo, formatStr) [following comment out lines are 'old style']
+    # dateNthDaysAgo = numberOfDaysAgo.day.ago.strftime(format=formatStr)
+    dateNthDaysAgo = numberOfDaysAgo.day.ago.strftime(format= "%m/%d/%Y")
+    return dateNthDaysAgo
+  end
+
+  def getDateNthDaysFromNow(numberOfDaysFromNow)
+    dateNthDaysFromNow = numberOfDaysFromNow.day.from_now.strftime(format= "%m/%d/%Y")
+    return dateNthDaysFromNow
+  end
+
+  # DATE [months]
+  def getDateNthMonthsAgo(numberOfDaysAgo)
+  dateNthDaysAgo = numberOfDaysAgo.month.ago.strftime(format= "%m/%d/%Y")
+  return dateNthDaysAgo
+  end
+
+  def getDateNthMonthsFromNow(numberOfDaysFromNow)
+    dateNthDaysFromNow = numberOfDaysFromNow.month.from_now.strftime(format= "%m/%d/%Y")
+    return dateNthDaysFromNow
+  end
+
+
+  # DATE [Years]
+  def getDateNthYearsAgo(numberOfDaysAgo)
+    dateNthDaysAgo = numberOfDaysAgo.year.ago.strftime(format= "%m/%d/%Y")
+    return dateNthDaysAgo
+  end
+
+  def getDateNthYeasFromNow(numberOfYearsFromNow)
+    dateStr = numberOfYearsFromNow.year.from_now.strftime(format= "%m/%d/%Y")
+    return dateStr
+  end
+
+##############################################   TIME  #############################################
+
+  def setTime( cssPath, timeStr)
+    # Provide the time on the timeStr and add the Css path
+    time = Time.parse(timeStr);
+    findElement(:css,cssPath).send_keys(time.strftime("%I"))
+    sleep 0.5
+    findElement(:css,cssPath).send_keys [:shift, ";"]
+    findElement(:css,cssPath).send_keys(time.strftime("%M %p"))
+  end
+
+# Verify the date format in from the field
+# * expect(isTimeFormatValid?("01:03 PM")).to eq(true)
+  def isTimeFormatValid?(timeStr)
+    begin
+      time = DateTime.strptime(timeStr, format= "%I:%M %p")
+      puts "[isTimeFormatValid?] Time is " + time.to_s
+      return true
+    rescue Exception=>e
+      return false
+    end
+  end
+
+  # Force the time to change Using Java script
+  def selectTimeZone(timeZone)
+  #puts "Time Zone is " + timeZone
+  # *selectTimeZone("(-05:00) America/New_York (Eastern)")
+  Sleep 1
+  @driver.execute_script("$('[name=timeZone]').scope().preferences.timeZone = \"" + timeZone + "\"")
+  @driver.execute_script("$('[name=timeZone]').scope().$digest()")
+end
+
+
+
+
+
+
+
+
+end
