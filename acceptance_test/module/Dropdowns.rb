7292d89c0269fc59f119c0f22d26440cfe724710
mizanur09 <mizanur.rahman0918@gmail.com>
2018-10-08 17:46:06 -0400

    Update Dropdown Modual and added MomgoDB

--- acceptance_test/module/Dropdowns.rb
+++ acceptance_test/module/Dropdowns.rb
@@ -1,4 +1,5 @@
 require_relative '../Helpers_pages/globalized'
+include Globalized
 
 module DropdownFunctions
 
--- acceptance_test/module/MongoDB.rb
+++ acceptance_test/module/MongoDB.rb
@@ -1,8 +1,21 @@
 require_relative '../Helpers_pages/globalized'
+include Globalized
 
-module MONGODB
+include Mongo
+include MongoImport
+module MongoUtility
 
+  HOSTANDPORT = 'Please enter Host port number here'
 
+  def printAllCollections
+    connection = Mongo::Client.new([HOSTANDPORT])
+    connection.database_names.each do |name|
+      db = connection.use(name)
+      db.collections.each do |collection|
+        puts "#{name} - #{collection.name}"
+      end
+    end
+  end
 
 
 
