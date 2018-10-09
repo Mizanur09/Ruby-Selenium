b7dbf86b25ebaac1079bcd25219d0c96cd07866f
mizanur09 <mizanur.rahman0918@gmail.com>
2018-10-08 22:00:25 -0400

    Added OracleDB ID pass and some other info

--- acceptance_test/module/OracleDB.rb
+++ acceptance_test/module/OracleDB.rb
@@ -2,9 +2,15 @@ require_relative '../Helpers_pages/globalized'
 include Globalized
 
 module OracleDBUtility
+  QUARTZ_SCHEMA = "QUARTZ"
+  ORACLE_HOST = "10.2.2.3"
+  ORACLE_USER = "hadb"
+  ORACLE_DB_PASS = "Pass"
+  ORACLE_PORT = 1212
+  ORACLE_SID = "vamfdb"
+  ORACLE_CON_STRING ="(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=#{ORACLE_HOST})(PORT=#{ORACLE_PORT}))(CONNECT_DATA=(SERVER=DEDICATED)(SID=#{ORACLE_SID})))"
 
-
-
+  
 
 
 
